3d174333e4d03b46e4e28a89552f58e5b7b4e262
S M Ohidul Alam <contact@ohid.info>
2017-12-05 03:09:53 +0800

    Minor Changes in Settings

--- myguide/myguide/settings/local.py
+++ myguide/myguide/settings/local.py
@@ -2,3 +2,9 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+# SECRET CONFIGURATION
+# ------------------------------------------------------------------------------
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+# Note: This key only used for development and testing.
+SECRET_KEY = env('DJANGO_SECRET_KEY', default='kl^biaul9klceb%@=mvy94)m@^f-c9v=4g+%t#zptzy403-^f6')
