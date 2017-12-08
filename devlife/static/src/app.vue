fd13c725db6df9a80e4cab9e03b7edcc98f7db3f
S M Ohidul Alam <contact@ohid.info>
2017-12-08 19:12:29 +0800

    Add app.vue in static/src

---
+++ devlife/static/src/app.vue
@@ -0,0 +1,20 @@
+<script>
+
+  export default {
+    components: {
+
+    },
+    data: {
+
+    },
+    computed: {
+
+    },
+    methods: {
+
+    },
+    created(){
+      
+    }
+  }
+</script>
