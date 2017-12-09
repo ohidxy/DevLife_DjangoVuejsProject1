a0f7d91fa737be3a2360daed891592210480784a
S M Ohidul Alam <contact@ohid.info>
2017-12-09 08:59:41 +0800

    Add Register Form through vuejs

--- devlife/static/src/app.vue
+++ devlife/static/src/app.vue
@@ -2,10 +2,11 @@
 import URI from 'urijs'
 
 import LoginForm from './login/loginForm.vue'
-
+import RegisterForm from './register/registerForm.vue'
 export default {
   components: {
     LoginForm,
+    RegisterForm,
   },
   data: {
 
---
+++ devlife/static/src/register/registerForm.vue
@@ -0,0 +1,18 @@
+<style lang="scss" scoped>
+
+</style>
+
+<template>
+  <div>
+    <h1>Register Here</h1>
+    <input type="text" placeholder="Username">
+    <input type="password" placeholder="Password">
+    <button type="submit">Register Now</button>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
