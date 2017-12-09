e2788ec49253b03c91fbc8bd160528513e4fce60
S M Ohidul Alam <contact@ohid.info>
2017-12-10 00:17:11 +0800

    Add column to shrink Login and Register form

--- devlife/static/src/login/loginForm.vue
+++ devlife/static/src/login/loginForm.vue
@@ -4,18 +4,24 @@
 
 <template>
   <div>
-    <h1>Login Here</h1>
-    <form action="">
-      <div class="form-group">
-        <label for="username">Username</label>
-        <input class="form-control" type="text" id="username" placeholder="Username">
+    <div class="row">
+      <div class="col-md-3"></div>
+      <div class="col-md-6">
+        <h1>Login Here</h1>
+        <form action="">
+          <div class="form-group">
+            <label for="username">Username</label>
+            <input class="form-control" type="text" id="username" placeholder="Username">
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input class="form-control" type="password" id="password" placeholder="Password">
+          </div>
+          <button class="btn btn-primary" type="submit">Login</button>
+        </form>
       </div>
-      <div class="form-group">
-        <label for="password">Password</label>
-        <input class="form-control" type="password" id="password" placeholder="Password">
-      </div>
-      <button class="btn btn-primary" type="submit">Login</button>
-    </form>
+      <div class="col-md-3"></div>
+    </div>
   </div>
 </template>
 
