f6cd785e0605b953ae39d973a3c5af19348a4222
S M Ohidul Alam <contact@ohid.info>
2017-12-11 21:12:33 +0800

    Remove column style in login and register forms' vue files

--- devlife/static/src/login/loginForm.vue
+++ devlife/static/src/login/loginForm.vue
@@ -6,31 +6,51 @@
 </style>
 
 <template>
-  <div>
-    <div class="row">
-      <div class="col-md-3"></div>
-      <div class="col-md-6 login-form">
-        <h1 class="text-center">Login Here</h1>
-        <form action="">
-          <div class="form-group">
-            <label for="username">Username</label>
-            <input class="form-control" type="text" id="username" placeholder="Username">
-          </div>
-          <div class="form-group">
-            <label for="password">Password</label>
-            <input class="form-control" type="password" id="password" placeholder="Password">
-          </div>
-          <button class="btn btn-primary" type="submit">Login</button>
-        </form>
+  <div class="login-form">
+    <h1 class="text-center">Login Here</h1>
+    <form action="">
+      <div class="form-group">
+        <label for="username">Username</label>
+        <input 
+          class="form-control" 
+          type="text" 
+          id="username" 
+          placeholder="Username"
+          v-model="email"
+          ref="loginInput"
+        >
       </div>
-      <div class="col-md-3"></div>
-    </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input 
+          class="form-control" 
+          type="password" 
+          id="password" 
+          placeholder="Password"
+          v-model="password"
+        >
+      </div>
+      <button 
+        class="btn btn-primary" 
+        type="submit"
+      >
+        Login
+      </button>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      email: '',
+      password: '',
+    }
+  },
+  mounted() {
+    this.$refs.loginInput.focus()
+  },
 }
 </script>
 
--- devlife/static/src/register/registerForm.vue
+++ devlife/static/src/register/registerForm.vue
@@ -6,34 +6,61 @@
 </style>
 
 <template>
-  <div>
-    <div class="row">
-      <div class="col-md-3"></div>
-        <div class="col-md-6 register-form">
-          <h1 class="text-center">Register Here</h1>
-          <form action="">
-            <div class="form-group">
-              <label for="email">Email</label>
-              <input class="form-control" type="text" id="email" placeholder="Email">
-            </div>
-            <div class="form-group">
-              <label for="password">Password</label>
-              <input class="form-control" type="password" id="password" placeholder="Password">
-            </div>
-            <div class="form-group">
-              <label for="confirm_password">Confirm Password</label>
-              <input class="form-control" type="password" id="confirm_password" placeholder="Password">
-            </div>
-            <button class="btn btn-primary" type="submit">Register</button>
-          </form>
-        </div>
-      <div class="col-md-3"></div>
-    </div>
+  <div class="register-form">
+    <h1 class="text-center">Register Here</h1>
+    <form action="">
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input 
+          class="form-control" 
+          type="text" 
+          id="email" 
+          placeholder="Enter your email here"
+          v-model="email"
+          ref="registerInput"
+        >
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input 
+          class="form-control" 
+          type="password" 
+          id="password"
+          placeholder="Enter a password"
+          v-model="password1"
+        >
+      </div>
+      <div class="form-group">
+        <label for="confirm_password">Confirm Password</label>
+        <input 
+          class="form-control" 
+          type="password" 
+          id="confirm_password" 
+          placeholder="Enter the password again."
+          v-model="password2"
+        >
+      </div>
+      <button 
+        class="btn btn-primary" 
+        type="submit"
+      >
+        Register
+      </button>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      email: null,
+      password1: null,
+      password2: null,
+    }
+  },
+  mounted() {
+    this.$refs.registerInput.focus()
+  }  
 }
 </script>
