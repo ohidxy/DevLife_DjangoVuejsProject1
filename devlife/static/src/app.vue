ca5217346914b41d20320b9ce60745b7d87cc4c1
S M Ohidul Alam <contact@ohid.info>
2017-12-13 08:15:09 +0800

    Add Sidebar Navigation in User Panel (Structure only)

--- devlife/static/src/app.vue
+++ devlife/static/src/app.vue
@@ -3,10 +3,12 @@ import URI from 'urijs'
 
 import LoginForm from './login/loginForm.vue'
 import RegisterForm from './register/registerForm.vue'
+import Navigation from './navigation/navigation.vue'
 export default {
   components: {
     LoginForm,
     RegisterForm,
+    Navigation,
   },
   data: {
 
---
+++ devlife/static/src/navigation/navigation.vue
@@ -0,0 +1,40 @@
+<style lang="scss" scoped>
+  .navigation {
+    margin: 3rem 0rem 1rem 0rem;
+  }
+  
+</style>
+
+<template>
+  <div class="navigation">
+    <ul class="list-group"> 
+      <li class="list-group-item">
+        <a href="#">My Contact</a>
+      </li>
+      <li class="list-group-item">
+        <a href="#">My Notes</a>
+      </li>
+      <li class="list-group-item">
+        <a href="#">Cheat Sheet</a>
+      </li>
+      <li class="list-group-item">
+        <a href="#">Important Links</a>
+      </li>
+      <li class="list-group-item">
+        <a href="#">Logbook</a>
+      </li>
+      <li class="list-group-item">
+        <a href="#">Files</a>
+      </li>
+      <li class="list-group-item">
+        <a href="#">Trends</a>
+      </li>
+    </ul>
+  </div>    
+</template>
+
+<script>
+export default {
+  
+}
+</script>
