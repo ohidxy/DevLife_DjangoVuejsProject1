5823f60ecfd30496f1510b77e56fdc048ca6fd1e
S M Ohidul Alam <contact@ohid.info>
2017-12-21 00:19:09 +0800

    Add Contact Serializer from contact model

---
+++ devlife/contact/serializers.py
@@ -0,0 +1,7 @@
+from .models import Contact
+from rest_framework import serializers
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact 
+        exclude = ('id', 'user')       
