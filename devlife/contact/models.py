7977f1c12fd4c1654999189e28a9acc356f61a8b
S M Ohidul Alam <contact@ohid.info>
2017-12-20 13:35:12 +0800

    Add contact model in models.py

--- devlife/contact/models.py
+++ devlife/contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    hp_no = models.CharField(max_length=128)
+    twitter = models.URLField()
+    github = models.URLField()
+    company = models.CharField(max_length=255)
