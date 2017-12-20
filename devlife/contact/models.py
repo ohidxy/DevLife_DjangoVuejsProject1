ccebb9341ba8b461baf443b9e7fe57187b3587b0
S M Ohidul Alam <contact@ohid.info>
2017-12-21 00:09:57 +0800

    Add user as a foreign key into contact model

--- devlife/contact/models.py
+++ devlife/contact/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 
 class Contact(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default='')
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField()
