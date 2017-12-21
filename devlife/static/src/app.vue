4116b92856f006d04e92099dad7aa90f8b3a8914
S M Ohidul Alam <contact@ohid.info>
2017-12-21 10:37:52 +0800

    Add viewContact.vue as a component

--- devlife/static/src/app.vue
+++ devlife/static/src/app.vue
@@ -3,11 +3,14 @@
 import LoginForm from './login/loginForm.vue'
 import RegisterForm from './register/registerForm.vue'
 import Navigation from './navigation/navigation.vue'
+import ViewContact from './contact/viewContact.vue'
+
 export default {
   components: {
     LoginForm,
     RegisterForm,
     Navigation,
+    ViewContact,
   },
   data: {
 
---
+++ devlife/static/src/contact/viewContact.vue
@@ -0,0 +1,48 @@
+<style lang="scss" scoped>
+  .view-contact {
+    margin: 3rem 0rem 1rem 0rem;
+  }
+</style>
+
+<template>
+<div class="view-contact">
+  <div class="text-right" style="margin-bottom:0.1rem">
+    <button class="btn btn-primary">Add Contact</button>
+  </div>
+  <div class="text-center" >
+    <table class="table table-striped table-bordered table-hover">
+      <thead>
+        <tr>
+          <th>Full Name</th>
+          <th>Email</th>
+          <th>HP No</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>John Doe</td>
+          <td>john@example.com</td>
+          <td>01115633567</td>
+        </tr>
+        <tr>
+          <td>Mary Moe</td>
+          <td>mary@example.com</td>
+          <td>0857343533</td>
+        </tr>
+        <tr>
+          <td>July Dooley</td>
+          <td>july@example.com</td>
+          <td>0156968943</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
