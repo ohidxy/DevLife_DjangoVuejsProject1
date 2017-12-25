11becbf37f68ed0ddfb14a7fbfaaae49ec9a1c72
S M Ohidul Alam <contact@ohid.info>
2017-12-25 12:01:45 +0800

    Serve scss & bootstrap through webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,21 @@
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const srcDir = path.resolve(__dirname, './devlife/static/src')
+
+function getPostcssLoaderPlugins () {
+  return [require('precss'), require('autoprefixer')]
+}
+const postcssLoaderOptions = {
+  plugins: getPostcssLoaderPlugins
+}
+const sassLoaderOptions = {
+  includePaths: [srcDir]
+}
+
 
 module.exports = {
   entry: {
-    index: ['./devlife/static/src/index.js', /* './devlife/static/src/index.scss' */ ]
+    index: ['./devlife/static/src/index.js', './devlife/static/src/index.scss']
   },
   watch: true,
   output: {
@@ -16,21 +29,39 @@ module.exports = {
   },
   module: {
     loaders: [
-    {
-      test: /\.js?$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      query: {
-        presets:['env']
-      }
-    },
-    {
-      test: /\.vue$/,
-      loader: 'vue-loader',
-      query: {
-        presets:['env']
-      }
-    },
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets:['env']
+        }
+      },
+
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        query: {
+          presets:['env']
+        }
+      },
+
+      {
+        test: /\.(scss|css)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            {loader: 'postcss-loader', options: postcssLoaderOptions},
+            {loader: 'sass-loader', options: sassLoaderOptions}
+          ]
+        })
+      },
+
     ]
-  }
+  },
+
+  plugins: [ 
+    new ExtractTextPlugin( "index.css" )
+  ]
 }
