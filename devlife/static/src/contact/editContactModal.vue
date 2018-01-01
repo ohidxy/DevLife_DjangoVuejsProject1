ef43dac6422ea68f40926867d93f622110a368ab
S M Ohidul Alam <contact@ohid.info>
2018-01-01 18:55:16 +0800

    Add fields in  'Edit Contact' component

--- devlife/static/src/contact/editContactModal.vue
+++ devlife/static/src/contact/editContactModal.vue
@@ -15,7 +15,59 @@
             </button>
           </div>
           <div class="modal-body">
-            
+            <form action="">
+              <div class="form-group">
+                <input 
+                  class="form-control" 
+                  type="text" 
+                  placeholder="First Name *"
+                  required
+                >
+              </div>
+              <div class="form-group">
+                <input 
+                  class="form-control" 
+                  type="text" 
+                  placeholder="Last Name *" 
+                  required
+                >
+              </div>
+              <div class="form-group">
+                <input 
+                  class="form-control" 
+                  type="text" 
+                  placeholder="Email *"
+                  required
+                >
+              </div>
+              <div class="form-group">
+                <input 
+                  class="form-control" 
+                  type="text" 
+                  placeholder="HP No (Handphone No)"
+                >
+              </div>
+              <div class="form-group">
+                <input 
+                  class="form-control" 
+                  type="text" 
+                  placeholder="Twitter (URL)"
+                >
+              </div>
+              <div class="form-group">
+                <input 
+                  class="form-control" 
+                  type="text" 
+                  placeholder="Github (URL)"
+                >
+              </div>
+              <div class="form-group">
+                <input 
+                  class="form-control" 
+                  type="text" 
+                  placeholder="Company (Optional)"
+                >
+              </div>
 
               <div class="text-right">
                 <button 
@@ -24,7 +76,14 @@
                 >
                 Save
                 </button>
+                <button 
+                  class="btn btn-danger btn-right" 
+                  type="submit"
+                >
+                Delete
+                </button>
               </div>
+            </form>
           
             <p class="text-center">* - mandatory field</p>
           </div>
@@ -43,13 +102,29 @@ axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 export default {
   data () {
     return {
-
+      single_contact_data: null,
+      first_name: null,
+      last_name: null,
+      email: null,
     }
   },
   props: {
-
+    apiUrl: {
+      type: String,
+      required: true
+    }
   },
   methods: {
+    view_single_contact(){
+      // Make a request for a user with a given ID
+      axios.get(this.apiUrl)
+        .then((res) => {
+          this.single_contact_data = res.data
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   }
 }
 </script>
