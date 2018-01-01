66384f14249f9605dfbd7fc49f2ee14bf387177b
S M Ohidul Alam <contact@ohid.info>
2018-01-01 20:24:17 +0800

    Add contactDashboard.vue component

---
+++ devlife/static/src/dashboard/contactDashboard.vue
@@ -0,0 +1,22 @@
+<style lang="scss" scoped>
+  .contact-dashboard {
+    border: solid 0.01rem rgb(214, 214, 214);
+    padding: 1rem;
+    background-color: rgb(250, 255, 226);
+  }
+  .faded-text {
+    color: rgb(139, 139, 139);
+  }
+</style>
+
+<template>
+  <div class="contact-dashboard text-center">
+    <h2> <i class="fa fa-phone-square" aria-hidden="true"></i> Contact</h2>
+    <p class="faded-text"> <strong> 56 Contacts saved </strong></p>
+  </div>
+</template>
+
+<script>
+
+</script>
+
