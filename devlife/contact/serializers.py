d05986364a69491e48b2f53c92b99dbffb7c1e6f
S M Ohidul Alam <contact@ohid.info>
2018-01-01 18:51:24 +0800

    Add Create method in contact serializer class

--- devlife/contact/serializers.py
+++ devlife/contact/serializers.py
@@ -6,3 +6,9 @@ class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact 
         fields = ('__all__')
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Contact` instance, given the validated data.
+        """
+        return Contact.objects.create(**validated_data)
