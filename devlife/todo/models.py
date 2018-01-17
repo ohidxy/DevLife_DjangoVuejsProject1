6370f8c7e9749569ce7384c21c7a4d10625e1df4
S M Ohidul Alam <contact@ohid.info>
2018-01-18 07:31:32 +0800

    Add todo model

--- devlife/todo/models.py
+++ devlife/todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    due_date = models.DateTimeField()
+    active_status = models.BooleanField()
