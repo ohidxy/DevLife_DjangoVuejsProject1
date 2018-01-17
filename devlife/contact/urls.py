0e9367cb7c587ffa6ea4376e657931e088dbe515
S M Ohidul Alam <contact@ohid.info>
2018-01-17 18:10:54 +0800

    Add todo app

--- devlife/contact/urls.py
+++ devlife/contact/urls.py
@@ -3,6 +3,6 @@ from django.views.generic import TemplateView
 from . import views
 urlpatterns = [
     url(r'^$', views.contact_page, name='contact_page'),
-    url(r'^api/$', views.ContactData.as_view(), name="contact_data_api"),  # this is for api only. it doesn't relate to any templates
-    url(r'^api/(?P<pk>[0-9]+)/$', views.ContactDetail.as_view(), name="contact_detail"),  
+    url(r'^rest-api/$', views.ContactData.as_view(), name="contact_data_api"),  # this is for api only. it doesn't relate to any templates
+    url(r'^rest-api/(?P<pk>[0-9]+)/$', views.ContactDetail.as_view(), name="contact_detail"),  
 ]
