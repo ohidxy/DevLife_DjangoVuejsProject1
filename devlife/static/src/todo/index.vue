a9fcbfe905c1632843e9f9bc65f295e3c6277d26
S M Ohidul Alam <contact@ohid.info>
2018-01-18 17:38:21 +0800

    Rearrange attributes of button

--- devlife/static/src/todo/index.vue
+++ devlife/static/src/todo/index.vue
@@ -9,10 +9,18 @@
     <div class="button-panel">
       <ul class="nav nav-tabs" role="tablist">
         <li class="nav-item">
-          <button @click="selectedComponent = 'all-tasks'" class="nav-link active" data-toggle="tab" href="#home" role="tab">All Tasks</button>
+          <button 
+            @click="selectedComponent = 'all-tasks'" 
+            class="nav-link active" data-toggle="tab" href="#home" role="tab"
+            >All Tasks
+          </button>
         </li>
         <li class="nav-item">
-          <button @click="selectedComponent = 'add-task'" class="nav-link" data-toggle="tab" href="#profile" role="tab">Add Task</button>
+          <button 
+            @click="selectedComponent = 'add-task'" 
+            class="nav-link" data-toggle="tab" href="#profile" role="tab"
+            >Add Task
+          </button>
         </li>
       </ul>
     </div>
